48ee71b060093273e915ef7de9a5b4d47b777c34
Florian EVEN <feven@redhat.com>
2022-09-19 09:39:03 +0200

    [FEN] refactor: add run section

--- hello.go
+++ hello.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	if len(os.Args) < 2 {
+		runPrompt()
+	} else {
+		runFile(os.Args[1])
+	}
+}
+
+func runPrompt() {
+	fmt.Println("<")
+
+}
+
+func runFile(path string) {
+	fmt.Println(path)
 }
